0b9181824ad6d87480b102556568b022120777db
RAÍSSA PEREIRA DA SILVA BEZERRA <raissa.bez08@outlook.com>
2026-03-16 09:51:55 -0300

    Corrigindo código da forma que o professor desejou

--- App.js
+++ App.js
@@ -8,70 +8,44 @@ import {
   Pressable
 } from 'react-native';
 
-import { SafeAreaView } from 'react-native-safe-area-context';
-
-import { useState } from 'react';
+// import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
-
-  const [nome, setNome] = useState('');
-  const [telefone, setTelefone] = useState('');
-  const [email, setEmail] = useState('');
-  const [dataNascimento, setData] = useState('');
-
-  const realizarCadastro = () => {
-    if (email === '' || nome === '' || telefone === '' || dataNascimento === '' ) {
-      Alert.alert("Atenção, por favor insira seus dados corretamente");
-    } else {
-      Alert.alert(`Cadastro realizado com sucesso. Bem-vindo(a) ${nome}!`)
-    }
-  }
   return (
-    <SafeAreaView style={styles.container}>
+    <View style={styles.container}>
       <View style={styles.conteudoPrincipal}>
         <StatusBar style="auto" />
-        <Text style={{fontSize: 20, fontWeight: 'bold'}}>CADASTRO DE USUÁRIO</Text>
+        <Text style={{ fontSize: 20, fontWeight: 'bold' }}>
+          CADASTRO DE USUÁRIO
+        </Text>
         <View style={styles.conteudoSecundario}>
-
           <Text style={styles.texto}>Nome</Text>
-          <TextInput
-            style={styles.input}
-            placeholder = "Digite seu nome"
-            value={nome}
-            onChangeText= {setNome}
-          />
+          <TextInput style={styles.input} placeholder="Digite seu nome" />
 
           <Text style={styles.texto}>Email</Text>
-          <TextInput
-            style={styles.input}
-            placeholder = "Digite seu email"
-            value={email}
-            onChangeText= {setEmail}
-          />
+          <TextInput style={styles.input} placeholder="Digite seu email" />
 
           <Text style={styles.texto}>Telefone</Text>
-          <TextInput
-            style={styles.input}
-            placeholder = "Digite seu telefone"
-            value={telefone}
-            onChangeText= {setTelefone}
-          />
+          <TextInput style={styles.input} placeholder="Digite seu telefone" />
 
           <Text style={styles.texto}>Data de nascimento</Text>
           <TextInput
             style={styles.input}
-            placeholder = "Digite sua data de nascimento"
-            value={dataNascimento}
-            onChangeText= {setData}
+            placeholder="Digite sua data de nascimento"
           />
         </View>
         <Pressable
-          style={styles.botao}
-          onPress={realizarCadastro}>
-          <Text style={styles.textoBotao}>Salvar</Text>
+          style={({ pressed }) => [
+            { backgroundColor: pressed ? '#FF91A4' : '#EFDECD' },
+            styles.botao,
+          ]}
+          onPress={() =>
+            Alert.alert('Success', 'Seu cadastro foi salvo com sucesso')
+          }>
+          <Text>Salvar</Text>
         </Pressable>
       </View>
-    </SafeAreaView>
+    </View>
   );
 }
 
@@ -85,7 +59,7 @@ const styles = StyleSheet.create({
   texto: {
     fontSize: 17,
     textAlign: 'center',
-    padding: 5
+    padding: 5,
   },
   conteudoPrincipal: {
     backgroundColor: '#F4C2C2',
@@ -94,7 +68,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     borderRadius: 15,
     elevation: 15,
-    padding: 20
+    padding: 20,
   },
   conteudoSecundario: {
     backgroundColor: '#EFDECD',
@@ -105,7 +79,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 10,
-    elevation: 10
+    elevation: 10,
   },
   input: {
     backgroundColor: '#F4C2C2',
@@ -113,14 +87,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 10,
-    elevation: 5
+    elevation: 5,
   },
   botao: {
-    backgroundColor: '#EFDECD',
     padding: 15,
     borderRadius: 15,
     width: '95%',
     alignItems: 'center',
-    elevation: 10
-  }
+    elevation: 10,
+  },
 });
